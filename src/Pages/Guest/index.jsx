3fe96d89ea760f3e2d4386efe6135737b207b751
somin <smhs12@naver.com>
2022-03-12 23:03:28 +0900

    방명록

--- src/Pages/Guest/index.jsx
+++ src/Pages/Guest/index.jsx
@@ -4,18 +4,25 @@ import React, { useEffect, useLayoutEffect, useRef } from "react";
 import Comment from "../../components/Comment";
 
 function Board() {
-  const { scene, nodes } = useGLTF("/gltf/board.glb");
-  const gltf = useGLTF("/gltf/board.glb");
+  const { scene, nodes } = useGLTF("/gltf/boardChanged.glb");
+  const gltf = useGLTF("/gltf/boardChanged.glb");
 
-  useEffect(() => {}, []);
   return (
-    <group scale={[0.1, 0.1, 0.1]} position={[1.3, 1, 1]}>
-      <primitive object={scene} rotation={[0, Math.PI * 0.82, 0]}></primitive>
+    <group
+      scale={[0.1, 0.1, 0.1]}
+      position={[2, 1, 1]}
+      rotation={[0, Math.PI * 0.82, 0]}
+    >
+      <primitive object={scene}></primitive>
       <mesh
         geometry={nodes["Plane"].geometry}
         material={nodes["Plane"].material}
       >
-        <Html position={[0, 3.5, 0]}>
+        <Html
+          transform
+          rotation={[0, Math.PI * 0.5, 0]}
+          position={[1.5, 3.6, 6.5]}
+        >
           <div className="wrapper">
             <Comment />
           </div>
@@ -25,8 +32,32 @@ function Board() {
   );
 }
 
+function Table() {
+  const { scene, nodes } = useGLTF("/gltf/pencilTable.glb");
+  const gltf = useGLTF("/gltf/pencilTable.glb");
+
+  useEffect(() => {
+    console.log(nodes);
+  }, []);
+
+  return (
+    <group
+      position={[2, 0.5, 0]}
+      scale={[0.3, 0.3, 0.3]}
+      rotation={[0, Math.PI * 0.5, 0]}
+    >
+      <primitive object={scene}></primitive>
+    </group>
+  );
+}
+
 const Guest = () => {
-  return <Board />;
+  return (
+    <>
+      <Board />
+      <Table />
+    </>
+  );
 };
 
 export default Guest;
--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,9 +1,92 @@
 import React from "react";
 import styled from "styled-components";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const testData = [
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+  {
+    name: "minseok",
+    id: 20172717,
+    content: "hello, i'm minseok.",
+  },
+  {
+    name: "junseo",
+    id: 20192717,
+    content: "hello, i'm junseo.",
+  },
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+  {
+    name: "somin",
+    id: 20182717,
+    content: "hello, i'm somin.",
+  },
+];
+const onClickRight = () => {
+  console.log("click");
+};
 const Comment = () => {
   return (
     <CommentContainer>
-      <div className="post-item">hello</div>
+      <MdOutlineKeyboardArrowLeft className="left" />
+      <div className="box-container">
+        {testData.map((test) => (
+          <div className="post-item">
+            <div className="top">
+              <div>{test.name}</div>
+              <div>{test.id}</div>
+            </div>
+            <hr />
+            <div>{test.content}</div>
+          </div>
+        ))}
+      </div>
+      <div onClick={onClickRight}>
+        <MdOutlineKeyboardArrowRight className="right" />
+      </div>
     </CommentContainer>
   );
 };
@@ -11,5 +94,34 @@ const Comment = () => {
 export default Comment;
 
 const CommentContainer = styled.div`
-  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1.5rem;
+  width: 145%;
+  height: 120%;
+  overflow: hidden;
+
+  .left,
+  .right {
+    font-size: 2rem;
+  }
+  .box-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+  .post-item {
+    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+    width: 16.5rem;
+    height: 9rem;
+    padding: 1rem;
+    background-color: #fbf8f1;
+    margin: 1rem;
+
+    .top {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+  /* background-color: black; */
 `;
