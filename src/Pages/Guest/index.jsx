3a9d1d8e8304aca4d2e825fdb07a50a8296fd7eb
somin <smhs12@naver.com>
2022-03-14 13:40:01 +0900

    테이블 수정

--- src/Pages/Guest/index.jsx
+++ src/Pages/Guest/index.jsx
@@ -1,6 +1,6 @@
-import { ScrollControls, useGLTF, useScroll, Html } from "@react-three/drei";
-import { useFrame, useLoader, useThree } from "@react-three/fiber";
-import React, { useEffect, useLayoutEffect, useRef } from "react";
+import { useGLTF, Html } from "@react-three/drei";
+// import { useFrame, useLoader, useThree } from "@react-three/fiber";
+import React, { useEffect } from "react";
 import Comment from "../../components/Comment";
 
 function Board() {
@@ -20,8 +20,9 @@ function Board() {
       >
         <Html
           transform
+          occlude
           rotation={[0, Math.PI * 0.5, 0]}
-          position={[1.5, 3.6, 6.5]}
+          position={[0.6, 3.6, 6.5]}
         >
           <div className="wrapper">
             <Comment />
@@ -33,23 +34,77 @@ function Board() {
 }
 
 function Table() {
-  const { scene, nodes } = useGLTF("/gltf/pencilTable.glb");
-  const gltf = useGLTF("/gltf/pencilTable.glb");
-
+  const { scene, nodes } = useGLTF("/gltf/table1.glb");
+  // const gltf = useGLTF("/gltf/papertable.glb");
   useEffect(() => {
     console.log(nodes);
   }, []);
 
   return (
-    <group
-      position={[2, 0.5, 0]}
-      scale={[0.3, 0.3, 0.3]}
-      rotation={[0, Math.PI * 0.5, 0]}
-    >
-      <primitive object={scene}></primitive>
+    <group scale={[0.01, 0.01, 0.01]} position={[0, 1, 0]}>
+      <mesh
+        material={nodes["table001"].material}
+        geometry={nodes["table001"].geometry}
+      ></mesh>
+      <mesh
+        material={nodes["table001_1"].material}
+        geometry={nodes["table001_1"].geometry}
+      ></mesh>
+      <mesh
+        material={nodes["table001_2"].material}
+        geometry={nodes["table001_2"].geometry}
+      ></mesh>
+      <mesh
+        material={nodes["table001_3"].material}
+        geometry={nodes["table001_3"].geometry}
+      ></mesh>
+      <mesh
+        material={nodes["table001_4"].material}
+        geometry={nodes["table001_4"].geometry}
+      ></mesh>
+      <mesh
+        material={nodes["table001_5"].material}
+        geometry={nodes["table001_5"].geometry}
+      ></mesh>
     </group>
   );
 }
+
+// function Pencil() {
+//   const { scene, nodes } = useGLTF("/gltf/pencil.glb");
+
+//   const onPencilClick = () => {
+//     console.log("click");
+//   };
+//   useEffect(() => {
+//     console.log(nodes);
+//   }, []);
+//   return (
+//     <group
+//       onClick={onPencilClick}
+//       scale={[0.01, 0.01, 0.01]}
+//       position={[1.3, 0.7, 0]}
+//       rotation={[0, 0, Math.PI]}
+//     >
+//       {/* <primitive object={scene} scale={[0.01, 0.01, 0.01]}></primitive> */}
+//       <mesh
+//         scale={[0.5, 3, 0.5]}
+//         material={nodes["Cylinder003"].material}
+//         geometry={nodes["Cylinder003"].geometry}
+//       ></mesh>
+//       <mesh
+//         scale={[0.5, 3, 0.5]}
+//         material={nodes["Cylinder003_1"].material}
+//         geometry={nodes["Cylinder003_1"].geometry}
+//       ></mesh>
+//       <mesh
+//         scale={[0.5, 3, 0.5]}
+//         material={nodes["Cylinder003_2"].material}
+//         geometry={nodes["Cylinder003_2"].geometry}
+//       ></mesh>
+//     </group>
+//   );
+// }
 
 const Guest = () => {
   return (
