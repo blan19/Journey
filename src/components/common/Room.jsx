6c851a70fd7ef4550c0c9f1e97fc80ecbb9fc9e9
somin <smhs12@naver.com>
2022-03-20 16:01:28 +0900

    road

--- src/components/common/Room.jsx
+++ src/components/common/Room.jsx
@@ -1,9 +1,14 @@
 import { useGLTF } from "@react-three/drei";
 
-export default function Room() {
+export default function Room({ position, rotation }) {
   const { scene } = useGLTF("/gltf/Chapter_1.glb");
 
   return (
-    <primitive position={[0, -1.2, 0]} scale={[0.1, 0.1, 0.1]} object={scene} />
+    <primitive
+      position={position}
+      rotation={rotation}
+      scale={[0.3, 0.3, 0.3]}
+      object={scene}
+    />
   );
 }
