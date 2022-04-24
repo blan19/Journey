19a573e3a988e2dc91d58e235a836f36490413ab
Junseo <oponize@naver.com>
2022-04-25 00:47:58 +0900

    Feat: Add Mobile Detect

--- src/View/Browser.jsx
+++ src/View/Browser.jsx
@@ -1,10 +1,74 @@
-import React from "react";
+import React, { useRef } from "react";
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { PointerLockControls } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+import { Player, Map, MouseReticle, Clouds, Ground } from "../components";
 
 const Browser = () => {
+  const controlsRef = useRef();
+  const isLocked = useRef(false);
   return (
-    <div>
-      <h1>Hi .... </h1>
-    </div>
+    <Canvas
+      frameloop="demand"
+      shadows
+      performance={{ min: 0.5, max: 1 }}
+      gl={{
+        alpha: false,
+        antialias: true,
+        toneMappingExposure: 1,
+        toneMapping: THREE.ACESFilmicToneMapping,
+        outputEncoding: THREE.sRGBEncoding,
+      }}
+      camera={{ fov: 45 }}
+      raycaster={{
+        computeOffsets: (_, { size: { width, height } }) => {
+          if (isLocked.current) {
+            return {
+              offsetX: width / 2,
+              offsetY: height / 2,
+            };
+          } else {
+            return null;
+          }
+        },
+      }}
+    >
+      <MouseReticle />
+      <PointerLockControls
+        onUpdate={() => {
+          if (controlsRef.current) {
+            controlsRef.current.addEventListener("lock", () => {
+              console.log("lock");
+              isLocked.current = true;
+            });
+            controlsRef.current.addEventListener("unlock", () => {
+              console.log("unlock");
+              isLocked.current = false;
+            });
+            // controlsRef.current.addEventListener("click", () => {
+            //   console.log("lock");
+            //   isLocked.current = false;
+            // });
+          }
+        }}
+        ref={controlsRef}
+      />
+      {/* <primitive object={new THREE.AxesHelper(100)} /> */}
+      {/* <gridHelper args={[100, 100]} /> */}
+
+      <ambientLight intensity={0.2} />
+
+      <directionalLight args={["gray", 1]} />
+      <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+      <fog color="#262837" near={1} far={15} />
+      <Physics gravity={[0, -30, 0]}>
+        <Clouds />
+        <Ground />
+        <Map control={controlsRef} />
+        <Player />
+      </Physics>
+    </Canvas>
   );
 };
 
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Clouds from "./Clouds";
+import { Player } from "./common/Player";
+import MouseReticle from "./common/MouseReticle";
+import Map from "./common/Map";
+import Ground from "./common/Ground";
+import Laptop from "./Laptop";
+
+export { Clouds, Player, MouseReticle, Map, Ground, Laptop };
