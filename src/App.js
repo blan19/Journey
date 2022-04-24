34489ceee82e39c9a99290effa2cc15378ccf5d5
Junseo <oponize@naver.com>
2022-04-25 01:55:47 +0900

    update lib

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Choose, When, OtherWise } from "./lib/Condition";
 import Mobile from "./View/Mobile";
 import Browser from "./View/Browser";
-import "./styles.css";
 import { isMobile } from "react-device-detect";
+import "./styles.css";
 
 const App = () => {
   return (
