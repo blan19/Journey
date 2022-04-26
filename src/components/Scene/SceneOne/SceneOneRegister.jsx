65522268477f75cbf07cb2aa22d7fe8279651956
JunSeo <oponize@naver.com>
2022-04-26 12:13:49 +0900

    feat: scene 1 laptop

--- src/components/Scene/SceneOne/SceneOneRegister.jsx
+++ src/components/Scene/SceneOne/SceneOneRegister.jsx
@@ -1,16 +1,124 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import useStore from "../../../store";
-import { RegisterContainer, RegisterHeader, RegisterMain } from "./styled";
+import {
+  RegisterContainer,
+  RegisterHeader,
+  RegisterLoading,
+  RegisterMain,
+} from "./styled";
+import { FiLoader } from "react-icons/fi";
+import { BiArrowBack } from "react-icons/bi";
+import { Choose, If, OtherWise, When } from "../../../lib/Condition";
 
 const Register = () => {
   const { register, setRegister } = useStore((state) => state);
+  const [show, setShow] = useState({ loading: false, show: false });
+
+  const onLoading = useCallback(() => {
+    setShow((prev) => ({ ...prev, loading: true }));
+    setTimeout(() => {
+      setShow((prev) => ({ ...prev, loading: false }));
+      setTimeout(() => {
+        setShow((prev) => ({ ...prev, show: true }));
+      }, 0);
+    }, 3000);
+  }, []);
+
+  const onBack = useCallback(() => {
+    setRegister();
+    setShow((prev) => ({ ...prev, loading: false, show: false }));
+  }, [setRegister]);
+
+  useEffect(() => {
+    return () => setShow((prev) => ({ ...prev, loading: false, show: false }));
+  }, []);
   return (
-    <RegisterContainer register={register} onClick={setRegister}>
-      <RegisterHeader>
-        <h1>최종 합격자 발표</h1>
-      </RegisterHeader>
-      <RegisterMain></RegisterMain>
-    </RegisterContainer>
+    <>
+      <If condition={show.loading}>
+        <RegisterLoading>
+          <FiLoader />
+        </RegisterLoading>
+      </If>
+      <RegisterContainer register={register}>
+        <RegisterHeader>
+          <BiArrowBack onClick={onBack} />
+          <h1>최종 합격자 발표</h1>
+        </RegisterHeader>
+        <RegisterMain>
+          <div className="register-info">
+            <p>지원자 정보</p>
+          </div>
+          <Choose>
+            <When condition={show.show}>
+              <div className="register-table">
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>모집 구분</th>
+                      <th>수시</th>
+                      <th>전형</th>
+                      <th>논술 우수자</th>
+                    </tr>
+                    <tr>
+                      <td>학과/학부</td>
+                      <td>글로벌미디어학부</td>
+                      <td>수험번호</td>
+                      <td>20223434</td>
+                    </tr>
+                    <tr>
+                      <td>주민번호</td>
+                      <td>123456-7890112</td>
+                      <td>지원자명</td>
+                      <td>슝슝이</td>
+                    </tr>
+                    <tr>
+                      <td>가상계좌번호</td>
+                      <td>123456-78-9011123</td>
+                      <td>등록예치금</td>
+                      <td>300,000</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="register-contents">
+                <p className="section-1">
+                  슝슝이님은 본교 논술우수자전형 글로벌미디어학부에{" "}
+                  <span>합격</span>하였습니다
+                </p>
+                <p className="section-2">
+                  합격을 진심으로 축하드리며, 합격자는 반드시{" "}
+                  <span>하단의 안내사항을 필독</span>하시기 바랍니다. 안내사항을
+                  확인하지 않을 시 발생하는 불이익에 대한 책임은 수험생 본인에게
+                  있습니다.
+                </p>
+                <p className="section-3">
+                  합격통지서와 예치금고지서는 재발급하지 않으므로 필요시
+                  출력하여 보관하셔야 하며, 확인(출력)은 반드시 pc를 이용하셔야
+                  합니다.
+                </p>
+              </div>
+            </When>
+            <OtherWise>
+              <div className="register-table">
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>수험번호</td>
+                      <td>20223434</td>
+                      <td>지원자명</td>
+                      <td>슝슝이</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="register-button">
+                <button onClick={onLoading}>합격 조회</button>
+              </div>
+            </OtherWise>
+          </Choose>
+        </RegisterMain>
+      </RegisterContainer>
+    </>
   );
 };
 
--- src/components/Scene/SceneOne/styled.jsx
+++ src/components/Scene/SceneOne/styled.jsx
@@ -1,12 +1,20 @@
 import styled from "styled-components";
+import { keyframes } from "styled-components";
+
+const rotation = keyframes`
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 
 const RegisterContainer = styled.div`
-  z-index: 9999;
+  z-index: 1001;
   width: 100%;
   height: 100vh;
   background-color: #fff;
   position: absolute;
   display: ${(props) => (props.register ? "block" : "none")};
+  opacity: ${(props) => (props.register ? 1 : 0)};
   top: 0;
   left: 0;
 `;
@@ -16,6 +24,14 @@ const RegisterHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+  svg {
+    position: absolute;
+    left: 10px;
+    font-size: 28px;
+    color: #fff;
+    cursor: pointer;
+  }
   h1 {
     color: #fff;
     margin: 0;
@@ -24,6 +40,87 @@ const RegisterHeader = styled.header`
   }
 `;
 
-const RegisterMain = styled.main``;
+const RegisterMain = styled.main`
+  padding-top: 10rem;
+  padding-left: 5rem;
+  padding-right: 5rem;
+  p {
+    margin: 0;
+  }
+  .register-info {
+    p {
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+  .register-table {
+    margin-top: 35px;
+    table {
+      width: 100%;
+    }
+    td {
+      border: 1px solid black;
+      text-align: center;
+      padding: 5px 0;
+    }
+    th {
+      border: 1px solid black;
+      text-align: center;
+      padding: 5px 0;
+    }
+  }
+  .register-contents {
+    margin-top: 50px;
+    span {
+      color: red;
+    }
+    font-size: 20px;
+    .section-1 {
+      color: #9c36b5;
+      font-weight: bold;
+    }
+    .section-2 {
+      color: #000;
+    }
+    .section-3 {
+      color: #9c36b5;
+      font-weight: bold;
+    }
+  }
+  .register-button {
+    margin-top: 200px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    button {
+      background-color: #009cca;
+      border: none;
+      border-radius: 3px;
+      padding: 7.5px 15px;
+      font-size: 15px;
+      color: #fff;
+      font-weight: bold;
+      cursor: pointer;
+    }
+  }
+`;
 
-export { RegisterContainer, RegisterHeader, RegisterMain };
+const RegisterLoading = styled.div`
+  z-index: 9999;
+  width: 100%;
+  height: 100vh;
+  background-color: #fff;
+  top: 0;
+  left: 0;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  svg {
+    animation: ${rotation} 2s infinite linear;
+    font-size: 28px;
+  }
+`;
+
+export { RegisterContainer, RegisterHeader, RegisterMain, RegisterLoading };
