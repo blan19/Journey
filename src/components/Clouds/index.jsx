ef365780b16d57056d0be3705a8c56ab557f29b5
JunSeo <oponize@naver.com>
2022-05-19 22:20:07 +0900

    feat: background sky

--- src/components/Clouds/index.jsx
+++ src/components/Clouds/index.jsx
@@ -6,12 +6,30 @@ const Clouds = (props) => {
     <group>
       {/* 하늘 */}
       <Sky
+        // azimuth={0.25}
+        // turbidity={8}
+        // mieCoefficient={0.005}
+        // mieDirectionalG={0.8}
+        // rayleigh={2}
+        // inclination={0.49}
+        // 1
+        // azimuth={0.25}
+        // turbidity={1.5}
+        // mieCoefficient={0.005}
+        // mieDirectionalG={0.3}
+        // rayleigh={2}
+        // inclination={0.495}
+        // 2
+        // azimuth={0.25}
+        // turbidity={1.5}
+        // mieCoefficient={0.005}
+        // mieDirectionalG={0.3}
+        // rayleigh={1.5}
+        // inclination={0.495}
+        // 3
         azimuth={0.25}
-        turbidity={8}
-        mieCoefficient={0.005}
-        mieDirectionalG={0.8}
-        rayleigh={2}
-        inclination={0.49}
+        turbidity={0}
+        rayleigh={0.25}
       />
       {/* 별 */}
       <Stars
